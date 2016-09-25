18bb964d23fc060e77267f110030c75d155f7198
SnakE <snake.scaly@gmail.com>
2016-09-25 23:30:13 +0300

    Deselect tile in list when tile ID is out of range

--- RisenautEditor/ViewModel/MainViewModel.cs
+++ RisenautEditor/ViewModel/MainViewModel.cs
@@ -18,6 +18,8 @@ namespace RisenautEditor.ViewModel
         private string title_base = "Risenaut Editor";
         private int selected_level_index = -1;
         private Level selected_level;
+        private int tile_brush = 1;
+        private int selected_tile_index = -1;
 
         public MainViewModel(IFileService file_service)
         {
@@ -49,6 +51,10 @@ namespace RisenautEditor.ViewModel
                     RaisePropertyChanged(() => Blocks);
                     RaisePropertyChanged(() => Levels);
                     RaisePropertyChanged(() => IsModified);
+                    if (TileBrush < Blocks.Count())
+                    {
+                        SelectedTileIndex = TileBrush;
+                    }
                     SaveFileCommand.RaiseCanExecuteChanged();
                     SaveFileAsCommand.RaiseCanExecuteChanged();
                 }
@@ -177,6 +183,34 @@ namespace RisenautEditor.ViewModel
         }
 
         public bool IsModified { get { return game_file != null ? game_file.IsModified : false; } }
+
+        public int TileBrush
+        {
+            get { return tile_brush; }
+            set
+            {
+                if (value < 0 || value > 255)
+                {
+                    throw new ArgumentOutOfRangeException("value", value, "Must be 0..255");
+                }
+                if (Set(() => TileBrush, ref tile_brush, value))
+                {
+                    SelectedTileIndex = value < Blocks.Count() ? value : -1;
+                }
+            }
+        }
+
+        public int SelectedTileIndex
+        {
+            get { return selected_tile_index; }
+            set
+            {
+                if (Set(() => SelectedTileIndex, ref selected_tile_index, value) && value >= 0 && value <= 255)
+                {
+                    TileBrush = value;
+                }
+            }
+        }
 
         public void OnClosing(object sender, CancelEventArgs e)
         {
